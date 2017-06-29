3c8718141148dd033bc131b2450a9b079489d627
Rohan <rohangho@live.com>
2017-06-29 07:33:51 +0530

    Added Button

--- Musician/app/src/main/java/com/example/android/musician/Artist.java
+++ Musician/app/src/main/java/com/example/android/musician/Artist.java
@@ -1,7 +1,10 @@
 package com.example.android.musician;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Artist extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Artist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
+
+        Button a=(Button)findViewById(R.id.song);
+        a.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                Intent numbersintent = new Intent(Artist.this,Detail.class);
+                startActivity(numbersintent);
+            }
+        });
     }
 }
--- Musician/app/src/main/java/com/example/android/musician/Music.java
+++ Musician/app/src/main/java/com/example/android/musician/Music.java
@@ -1,7 +1,13 @@
 package com.example.android.musician;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.id;
+import static android.os.Build.VERSION_CODES.M;
 
 public class Music extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class Music extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        Button a=(Button)findViewById(R.id.song);
+        a.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                Intent numbersintent = new Intent(Music.this,Detail.class);
+                startActivity(numbersintent);
+            }
+        });
     }
 }
